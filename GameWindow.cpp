d28b4f3e65a4b181a236e94ca13996df6766d06c
Emma <Emma.viktoria.marthinussen@gmail.com>
2025-03-20 16:16:37 +0100

    ferdig med tegning av underbrett

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -11,13 +11,16 @@ void GameWindow::drawGrid(AnimationWindow& win, const Field& Field){
         for (int x = 0; x < Field.getW(); x++){
 
             TDT4102::Color color = TDT4102::Color::grey;
+            TDT4102::Image numImage(numPic.at((*field.getField()[y])[x]));
+            
             if ((*field.getField()[y])[x] == -1){
                 color = TDT4102::Color::red;
+                win.draw_rectangle(TDT4102::Point{x * cellSize, y * cellSize}, cellSize-2, cellSize-2, color);
             } else{
                 color = TDT4102::Color::grey;
             }
-
-            win.draw_rectangle(TDT4102::Point(x * cellSize, y * cellSize), cellSize-2, cellSize-2, color);
+            win.draw_rectangle(TDT4102::Point{x * cellSize, y * cellSize}, cellSize-2, cellSize-2, color);
+            win.draw_image(TDT4102::Point{x * cellSize, y * cellSize}, numImage);
         }
     }
 }
